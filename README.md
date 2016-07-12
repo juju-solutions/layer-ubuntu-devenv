4651bc7ae568b7b18b101c260532e95eebab0b6f
Kevin W Monroe <kevin.monroe@canonical.com>
2016-07-12 15:58:45 +0000

    add database interfaces and send db data to the debug log; update readme with db bits

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 This layered charm provides the same functionality as the 'ubuntu' charm with
 revision control tools preinstalled, as well as relations to other
 developer-oriented charms. It is useful as both a development environment and
-an endpoint for testing charms like 'openjdk' and 'ibm-xlc'.
+an endpoint for testing charms like 'openjdk', 'ibm-xlc', and various
+databases.
 
 Source for this charm is available on
 [github](https://github.com/juju-solutions/layer-ubuntu-devenv).
 
 
 # Usage
+
+## Use Case 1
 
 An example use case for this charm is to test Java JRE or Java SDK
 providers. This charm supports the `java` interface and serves as a
@@ -20,6 +23,23 @@ subordinate. Deploy as follows:
     juju deploy openjdk
     juju add-relation ubuntu-devenv openjdk
 
+## Use Case 2
+
+Another use case is to test database charms. This charm supports the `db2`,
+`mysql`, and `pgsql` interfaces. Once related, connection information will
+be logged to the debug log. Deploy as follows:
+
+    juju deploy ubuntu-devenv
+    juju deploy mariadb
+    juju add-relation ubuntu-devenv mariadb
+
+
+# Verification
+
+Verify you see relation data (java version, database connection information,
+etc) in the debug log:
+
+    juju debug-log -i unit-ubuntu-devenv-0 --replay
 
 # Limitations
 
--- reactive/ubuntu-devenv.py
+++ reactive/ubuntu-devenv.py
@@ -23,8 +23,14 @@ def install():
 def update_status():
     """Update status based on related charm states."""
     ready_services = []
+    if is_state('db2.ready'):
+        ready_services.append('db2')
     if is_state('java.ready'):
         ready_services.append('java')
+    if is_state('mysql.available'):
+        ready_services.append('mysql')
+    if is_state('pgsql.database.available'):
+        ready_services.append('pgsql')
     if is_state('xlc.ready'):
         ready_services.append('xlc')
     if is_state('xlf.ready'):
@@ -37,6 +43,27 @@ def update_status():
     status_set('active', 'devenv ready%s' % (status_suffix))
 
 
+@when('ubuntu-devenv.installed', 'db2.connected')
+def send_db2_details(db2):
+    """
+    DB2 requires an ssh key to be sent from the client to the server before
+    the relation will become ready. We don't actually need ssh access for this
+    charm, so send an invalid string as our key.
+    """
+    db2.set_ssh_keys("Invalid")
+
+
+@when('ubuntu-devenv.installed', 'db2.ready')
+def log_db2_details(db2):
+    """Log pertinent DB2-related details."""
+    if (data_changed('db2.host', db2.get_db2_hostname())):
+        log("DB2 host: '%s'" % db2.get_db2_hostname())
+        log("DB2 port: '%s'" % db2.get_db2_port())
+        log("DB2 user: '%s'" % db2.get_dbusername())
+        log("DB2 pass: '%s'" % db2.get_dbuserpw())
+        log("DB2 db: '%s'" % db2.get_db2_dbnames())
+
+
 @when('ubuntu-devenv.installed', 'java.ready')
 def log_java_details(java):
     """Log pertinent Java-related details."""
@@ -45,3 +72,17 @@ def log_java_details(java):
 
         log("Java (%s) using JAVA_HOME: '%s'" %
             (java.java_version(), java.java_home()))
+
+
+@when('ubuntu-devenv.installed', 'mysql.available')
+def log_mysql_details(mysql):
+    """Log pertinent Mysql-related details."""
+    if (data_changed('mysql.host', mysql.host())):
+        log("Mysql connection string: '%s'" % mysql.connection_string())
+
+
+@when('ubuntu-devenv.installed', 'pgsql.database.available')
+def log_pgsql_details(pgsql):
+    """Log pertinent Postgres-related details."""
+    if (data_changed('pgsql.host', pgsql.host())):
+        log("Postgres connection string: '%s'" % pgsql.connection_string())
